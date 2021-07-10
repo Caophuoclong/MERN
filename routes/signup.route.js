8245e168e98f0de26da8ecb4e538ed7170e30d7c
cakcak2001 <caophuoclong1@gmail.com>
2021-07-10 12:06:22 +0700

    DELETE ALL

--- routes/signup.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const routes = express.Router();
-const signup = require('../controllers/signup.controller.js')
-
-routes.get('/', signup.get);
-routes.post('/',signup.post);
-
-module.exports = routes;
--- routes/user.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const routes = express.Router();
-const userController = require('../controllers/user.controller.js')
-
-routes.get('/', userController.get);
-routes.post('/',userController.post);
-
-module.exports = routes;
